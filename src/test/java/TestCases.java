bfdced96f90eab5253348d081feb668e7b5a9bce
darshan1494 <myEmail@example.com>
2024-05-29 13:57:52 +0530

    TestNGDemo_Maven

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    public void test1(){
+        System.out.println("I am inside test1 method");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("I am inside test2 method");
+    }
+}
